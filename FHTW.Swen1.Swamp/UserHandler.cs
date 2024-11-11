36836018d34d40538422298237c0074463f3e992
robbiblubber <alexander.lang@orf.at>
2024-11-11 17:21:08 +0100

    0.0.5 started UserHandler.

---
+++ FHTW.Swen1.Swamp/UserHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHTW.Swen1.Swamp
+{
+    public class UserHandler: Handler, IHandler
+    {
+        public override bool Handle(HttpSvrEventArgs e)
+        {
+            if(e.Path.StartsWith("users"))
+            {
+                // bla...
+
+                //e.Reply()
+                return true;
+            }
+            else if(e.Path.StartsWith("user"))
+            {
+                // bla
+                if(e.Method == "GET")
+                {
+                    // bla...
+                }
+
+                //e.Reply()
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
