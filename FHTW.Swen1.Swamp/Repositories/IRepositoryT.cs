2d459aa08e2242273874f1b20a575ec62d493145
robbiblubber <alexander.lang@orf.at>
2024-12-06 12:46:25 +0100

    0.0.12 started repository implementation.

---
+++ FHTW.Swen1.Swamp/Repositories/IRepositoryT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+namespace FHTW.Swen1.Swamp.Repositories
+{
+    public interface IRepository<T>
+    {
+        public T Get(object id);
+
+        public IEnumerable<T> GetAll();
+
+        public void Save(T obj);
+
+        public void Delete(T obj);
+    }
+}
---
+++ FHTW.Swen1.Swamp/Repositories/RepositoryT.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+
+namespace FHTW.Swen1.Swamp.Repositories
+{
+    public abstract class Repository<T>: IRepository<T>
+    {
+        private static IDbConnection? _DbConnection = null;
+
+        protected string _TABLE_NAME = string.Empty;
+
+
+
+        public virtual void Delete(T obj)
+        {
+            
+        }
+
+        public virtual T Get(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Save(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        protected static IDbConnection _Cn
+        {
+            get 
+            { 
+                if(_DbConnection == null)
+                {
+                    _DbConnection = new SQLiteConnection(@"Data Source = C:\home\projects\FHTW.Swen1.Swamp.NET\res\swamp.db; Version = 3;");
+                    _DbConnection.Open();
+                }
+
+                return _DbConnection; 
+            }
+        }
+    }
+}
